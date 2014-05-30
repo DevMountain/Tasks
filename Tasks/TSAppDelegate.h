2a0056dd378f0b15e63ece01b6fbceeb089abf34
Joshua Howland <development@jkhowland.com>
2014-05-30 00:24:39 -0600

    Initial Commit

---
+++ Tasks/TSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSAppDelegate.h
+//  Tasks
+//
+//  Created by Joshua Howland on 5/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tasks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tasks
+//
+//  Created by Joshua Howland on 5/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSAppDelegate class]));
+    }
+}
